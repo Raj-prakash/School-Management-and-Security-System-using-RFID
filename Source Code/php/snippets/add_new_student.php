ead15627102bc34f274cb71577c67518705b9d53
Rajesh S <89447867+Raj-prakash@users.noreply.github.com>
2021-08-24 17:26:38 +0530

    Create add_new_student.php

---
+++ Source Code/php/snippets/add_new_student.php
@@ -0,0 +1,5 @@
+?>
+  <form method="POST" action="http://localhost/wordpress/add-new-student/">
+<input type="text" placeholder="(eg: 1, 5, 50, 100)" required name="add_no"><br><br>
+<center><button class="alert-danger  form-control " type="submit">Generate Form</button></center>
+  </form>
