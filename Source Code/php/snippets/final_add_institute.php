b32a54409d3841b1a147672d7941ef4dfcac3b4f
Rajesh S <89447867+Raj-prakash@users.noreply.github.com>
2021-08-24 17:31:15 +0530

    Create final_add_institute.php

---
+++ Source Code/php/snippets/final_add_institute.php
@@ -0,0 +1,27 @@
+?>
+<form method="POST" action="http://localhost/wordpress/final-institute-add/">  
+  <table>
+  <tr>
+  <td>Institute ID</td>
+<td><input type="text" placeholder="eg: GRA101" required autofocus name="inst_id"></td>
+</tr>
+<tr>
+<td>Institute Name</td>
+<td><input type="text" placeholder="eg:Grace REACH Academy" required name="inst_name"></td>
+</tr>
+<tr>
+<td>Contact Number</td>
+<td><input type="tel" placeholder="eg: 7010375918" required name="connum"></td>
+</tr>
+<tr>
+<td>Address</td>
+<td><input type="text" placeholder="eg: Laipanganba, Hiyanglam, Manipur " required name="address"></td>
+</tr>
+<tr>  
+<td>Email</td>
+<td><input type="text" placeholder="eg: gra@gmail.com" required name="email"></td>
+</tr>
+ <tr><td colspan="2"><button type="submit">Add Institute</button></td></tr>
+  </table> 
+  
+ </form>
