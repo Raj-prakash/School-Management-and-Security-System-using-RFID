cd49f887a01bae083333e1ed388c451fce783b1b
Rajesh S <89447867+Raj-prakash@users.noreply.github.com>
2021-08-24 17:34:07 +0530

    Create inst_ae_1.php

---
+++ Source Code/php/snippets/inst_ae_1.php
@@ -0,0 +1,22 @@
+?>
+  <h2>
+<form method="POST" action="http://localhost/wordpress/add-into-attendance-log/">
+<lable>To add into attendance log...</label>
+<table>
+  <tr>
+  <td>Registration Number:</td>
+			<td><input type="text" name="regno"></td>
+<td><button type="submit">Add Into Attendance Log</button></td>
+</tr>
+</table>
+</form>
+<form method="POST" action="http://localhost/wordpress/edit-attendance-log/">
+    <lable>To edit the attendance log...</label>
+    <table>
+    <tr>
+<td>Registration Number:</td>
+<td><input type="text" name="regno"></td>
+<td><button type="submit">Edit Attendance Log</button></form></td>
+		</tr>
+	</table>
+</form></h2>
