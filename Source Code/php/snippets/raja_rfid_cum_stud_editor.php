538bb1ccfef5763136e29b3e13e94fd371e0080e
Rajesh S <89447867+Raj-prakash@users.noreply.github.com>
2021-08-24 17:48:16 +0530

    Create raja_rfid_cum_stud_editor.php

---
+++ Source Code/php/snippets/raja_rfid_cum_stud_editor.php
@@ -0,0 +1,16 @@
+?>
+<form method="post" action="http://localhost/wordpress/dev-view-edit-student/">
+  <table>
+  <tr>
+  <td>
+<center><h3><b>Regno :</b3></h3></td>
+  <td><input type="text" name="regno" placeholder="eg:A161419" autofocus required></center></br></br>
+  </td>
+  </tr>
+  <tr>
+   <td colspan="2" style="text-align:center;">
+  <center><button type="submit">Search</button>
+  </td>
+  </tr>
+  </table>
+</form>
