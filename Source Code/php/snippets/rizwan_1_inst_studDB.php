70904dfc9b6a592d96cf8c29a82030d54615f282
Rajesh S <89447867+Raj-prakash@users.noreply.github.com>
2021-08-24 17:49:09 +0530

    Create rizwan_1_inst_studDB.php

---
+++ Source Code/php/snippets/rizwan_1_inst_studDB.php
@@ -0,0 +1,91 @@
+$con=$_SESSION['con'];
+if(isset ($_POST['but1']))
+{
+  $but1=$_POST['but1'];
+ $regno=$_POST['regno'];
+  if($but1=="update")
+  {
+  
+   $class=$_POST['class'];
+  $name=$_POST['sname'];
+  $section=$_POST['section'];
+  $f_m_name=$_POST['fname'];
+  $address=$_POST['address'];
+  $con_no=$_POST['no'];
+  $mail=$_POST['email'];
+  $qry="update student_master set name='$name', class='$class', section='$section', f_m_name='$f_m_name', address='$address', contact_no='$con_no', email_id='$mail' where regno='$regno'";
+  $result=mysqli_query($con,$qry);
+  if($result)
+  {
+   echo '<script language="javascript">';
+echo 'alert("Successfully Updated the Student Details")';
+echo '</script>';
+  }
+  
+  }
+  else if($but1=="delete")
+  {
+    $qry="delete from student_master where regno='$regno'";
+     $result=mysqli_query($con,$qry);
+  if($result)
+  {
+    echo '<script language="javascript">';
+echo 'alert("Successfully Deleted the Student Details!! Going back to the Student Selection Page!!");';
+echo 'window.location.href = "http://localhost/wordpress/student-database-viewer-editor/";';
+echo '</script>';
+  }
+  
+  }
+}
+$regno=$_POST["regno"];
+
+$qry="select * from student_master where regno='$regno'";
+			$result=mysqli_query($con,$qry);
+  if($result)
+   
+  $row=$result->fetch_assoc();
+  
+  $rfid_no=$row['rfid'];
+  $inst_id=$row['institute_id'];
+  $class=$row['class'];
+  $name=$row['name'];
+  $section=$row['section'];
+  $f_m_name=$row['f_m_name'];
+  $address=$row['address'];
+  $con_no=$row['contact_no'];
+  $mail=$row['email_id'];
+
+
+
+?>
+  <form method="POST">
+ <table>
+<tr><td>Reg No:</td><td><input type="text" readonly name="regno" value='<?php echo $regno; ?>'>
+</td></tr>
+
+<tr><td>RFID No:</td><td><input type="text" name="rfid" value='<?php echo $rfid_no;?>' readonly>
+</td></tr>
+  
+<tr><td>Name:</td><td><input type="text" name="sname" value='<?php echo $name; ?>' autofocus required></td></tr>
+
+<tr><td>Class:</td><td><input type="text" name="class"  value='<?php echo $class; ?>' required></td></tr>
+
+<tr><td>Section:</td><td><input type="text" name="section" value='<?php echo $section; ?>' required></td></tr>
+<tr><td>Father/Mother Name:</td><td><input type="text" name="fname" value='<?php echo $f_m_name; ?>' required></td></tr>
+
+
+
+<tr><td>Contact No:</td><td><input type="tel" name="no" value='<?php echo $con_no; ?>' required></td></tr>
+
+<tr><td>Address:</td><td><input type="text"  name='address' value='<?php echo $address; ?>' required></td></tr>
+
+<tr><td>Email:</td><td><input type="email" name="email" value='<?php echo $mail; ?>' required></td></tr>
+
+<tr>
+<td coldspan="2"><center><button type="submit" value="update" name="but1">Update</button>
+<a href="http://localhost/wordpress/student-database-viewer-editor/"><button type="button" value="goback" name="but1">Go Back</button></a>
+<button type="submit" value="delete" name="but1">Delete</button>
+  </td>
+  </tr></center>
+</table>
+</form>
